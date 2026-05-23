c20c298510d55b33f6d97b6aa06728a4c006460b
오성이 <hospi0539@naver.com>
2026-05-24 03:06:22 +0900

    mission3 clear

---
+++ week09/mission/mission3/src/slices/modalSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export interface ModalState {
+  isOpen: boolean;
+}
+
+const initialState: ModalState = {
+  isOpen: false,
+};
+
+const modalSlice = createSlice({
+  name: "modal",
+  initialState,
+  reducers: {
+    //1. modal open
+    openModal: (state) => {
+      state.isOpen = true;
+    },
+    //2. modal close
+    closeModal: (state) => {
+      state.isOpen = false;
+    },
+  },
+});
+
+export const { openModal, closeModal } = modalSlice.actions;
+
+const modalReducer = modalSlice.reducer;
+
+export default modalReducer;
